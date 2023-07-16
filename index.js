642a39c4ae99c6aa5051f58411a7d51561e4d7c8
Grzesiek <gzgrzegorzziemba@gmail.com>
2023-07-16 11:33:48 +0200

    Added styles and page start to look like sth

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const ejs = require("ejs");
-
+const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("main");
+});
+
+app.get("/subpage", (req, res) => {
+  res.render("subpage");
 });
 
 app.listen(3000, () => {
